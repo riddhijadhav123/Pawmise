35d0e8bf7456d793256f39046e8104143b6217f2
riddhijadhav123 <riddhi.18081@sakec.ac.in>
2025-10-20 09:28:59 +0530

    update server.js

--- pet_app/backend/server.js
+++ pet_app/backend/server.js
@@ -40,6 +40,4 @@ app.post('/api/payment/orders', async (req, res) => {
   }
 });
 
-// Server Listen
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+module.exports = app;
